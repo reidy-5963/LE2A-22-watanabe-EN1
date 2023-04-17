2eb58ec3425b7a97edd8ac035146e75aaba800ec
k022g0060@g.neec.ac.jp <k022g0060@g.neec.ac.jp>
2023-04-17 10:18:57 +0900

    Hello World

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
